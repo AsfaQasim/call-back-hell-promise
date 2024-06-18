780a8f55b131b8feb126bd7b9cb767d02dfcf35d
Asifa Muhammad Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-06-18 09:18:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+"use strict";
+// call back hell
+setTimeout(() => {
+    console.log("Called to HR");
+    setTimeout(() => {
+        console.log("HR to director");
+        setTimeout(() => {
+            console.log("Information update!");
+        }, 1000);
+    }, 2000);
+}, 2000);
+// Promise 
+// let promises = new Promise ((resolve, reject) => {
+//     console.log("Promise called");
+//     let obj = {
+//         name: "asfa",
+//         age: 21,
+//         class: "Thursday"
+//     }
+//     resolve(obj)
+//     reject("Something went wrong!")
+// })
+// promises.then((res) => console.log(res)).catch((err) => console.log(err));
